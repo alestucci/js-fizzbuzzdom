706f445554906c3d3af1c7a011a1c3bf0d613cdc
alestucci <alessandrotucci@gmail.com>
2022-02-23 16:47:06 +0100

    CSS classes definition

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ let output = document.querySelector(".container");
 for (let i = 1; i <= 100; i++) {
   switch (i % 15) {
     case 0:
-      output.innerHTML += `<div class="square">fizzbuzz</div>`;
+      output.innerHTML += `<div class="square sky-blue">fizzbuzz</div>`;
       break;
     case 3:
     case 6:
     case 9:
     case 12:
-      output.innerHTML += `<div class="square">fizz</div>`;
+      output.innerHTML += `<div class="square grey-green">fizz</div>`;
       break;
     case 5:
     case 10:
-      output.innerHTML += `<div class="square">buzz</div>`;
+      output.innerHTML += `<div class="square green">buzz</div>`;
       break;
     default:
-      output.innerHTML += `<div class="square">${i}</div>`;
+      output.innerHTML += `<div class="square sea">${i}</div>`;
       break;
   }
 }
