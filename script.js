f1fe82f2b22bb4bcd9c3628c4c332e58e2da0005
alestucci <alessandrotucci@gmail.com>
2022-02-23 17:29:45 +0100

    IF mode JS

--- script.js
+++ script.js
@@ -23,34 +23,58 @@ let output = document.querySelector(".container");
 }*/
 
 // 2nd solution
+// for (let i = 1; i <= 100; i++) {
+//   let box = document.createElement("div");
+//   box.classList.add("square");
+
+//   switch (i % 15) {
+//     case 0:
+//       box.classList.add("sky-blue");
+//       box.innerHTML = "fizzbuzz";
+//       output.append(box);
+//       break;
+//     case 3:
+//     case 6:
+//     case 9:
+//     case 12:
+//         box.classList.add("grey-green");
+//         box.innerHTML = "fizz";
+//         output.append(box);
+//       break;
+//     case 5:
+//     case 10:
+//         box.classList.add("green");
+//         box.innerHTML = "buzz";
+//         output.append(box);
+//       break;
+//     default:
+//         box.classList.add("sea");
+//         box.innerHTML = i;
+//         output.append(box);
+//       break;
+//   }
+// }
+
+// 3rd solution
 for (let i = 1; i <= 100; i++) {
   let box = document.createElement("div");
   box.classList.add("square");
 
-  switch (i % 15) {
-    case 0:
-      box.classList.add("sky-blue");
-      box.innerHTML = "fizzbuzz";
-      output.append(box);
-      break;
-    case 3:
-    case 6:
-    case 9:
-    case 12:
-        box.classList.add("grey-green");
-        box.innerHTML = "fizz";
-        output.append(box);
-      break;
-    case 5:
-    case 10:
-        box.classList.add("green");
-        box.innerHTML = "buzz";
-        output.append(box);
-      break;
-    default:
-        box.classList.add("sea");
-        box.innerHTML = i;
-        output.append(box);
-      break;
+  if (i % 15 == 0) {
+    box.classList.add("sky-blue");
+    box.innerHTML = "fizzbuzz";
+    output.append(box);
+  } else if (i % 3 == 0) {
+    box.classList.add("grey-green");
+    box.innerHTML = "fizz";
+    output.append(box);
+  } else if (i % 5 == 0) {
+    box.classList.add("green");
+    box.innerHTML = "buzz";
+    output.append(box);
+  } else {
+    box.classList.add("sea");
+    box.innerHTML = i;
+    output.append(box);
   }
 }
