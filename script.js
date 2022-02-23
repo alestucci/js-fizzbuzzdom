5c30a262b6301d42614a237ba5882a36cd87a335
alestucci <alessandrotucci@gmail.com>
2022-02-23 16:05:11 +0100

    Switch mode JS

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-for (let i = 0; i <= 100; i++) {
-    document.writeln(i);
-    
+let output = document.querySelector(".container");
+
+for (let i = 1; i <= 100; i++) {
+  switch (i % 15) {
+    case 0:
+      output.innerHTML += `<div class="square">fizzbuzz</div>`;
+      break;
+    case 3:
+    case 6:
+    case 9:
+    case 12:
+      output.innerHTML += `<div class="square">fizz</div>`;
+      break;
+    case 5:
+    case 10:
+      output.innerHTML += `<div class="square">buzz</div>`;
+      break;
+    default:
+      output.innerHTML += `<div class="square">${i}</div>`;
+      break;
+  }
 }
